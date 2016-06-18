4a59ce7122e7aca4d57eb209c0b4028f493d63fa
Mathieu CECCATO <mceccato@e1r12p8.42.fr>
2016-06-18 20:04:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var app		= require('express')(),
+	server 	= require('http').createServer(app),
+	io		= require('socket.io').listen(server),
+	ent		= require('ent'),		// htmlentities
+	// mysql 	= require('mysql'),
+	fs 		= require('fs');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname+"/views");
+
+var dir = __dirname;
+
+
+app.get('/', function(req, res) {
+	res.setHeader('Content-Type', 'text/html');
+	res.render('index', {dir: dir});
+});
+
+
+server.listen(8080);
