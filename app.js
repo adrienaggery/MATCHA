8b9a54ab450ee78e28104e5cac3784934ddcc915
Mathieu CECCATO <mceccato@e1r12p10.42.fr>
2016-06-21 19:40:44 +0200

    feuille de style integree

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var app		= require('express')(),
+var express = require('express'),
+	app		= express(),
 	server 	= require('http').createServer(app),
 	io		= require('socket.io').listen(server),
 	ent		= require('ent'),		// htmlentities
-	// mysql 	= require('mysql'),
+	mysql 	= require('mysql'),
 	fs 		= require('fs');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+"/views");
-
-var dir = __dirname;
+app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get('/', function(req, res) {
 	// res.setHeader('Content-Type', 'text/html');
-	res.render('index', {dir: dir});
+	res.render('pages/index');
 });
-
 
 server.listen(8080);
