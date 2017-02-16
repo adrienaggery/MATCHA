c2d5c7fe9da84680fe0af475c83813617e359af5
Mathieu <mceccato@student.42.fr>
2017-02-16 23:55:35 +0100

    fix flash message

--- routes/main.js
+++ routes/main.js
@@ -11,46 +11,45 @@ app.use(session({
 	cookie: { secure: false }
 }))
 
-// app.use(require('../middlewares/flash'))
+app.use(require('../middlewares/flash'))
 
 
-	app.route('/')
-		.get((req, res) => {
-			console.log(req.session.error)
+	app.get('/', (req, res) => { 
+
 			res.render('pages/index');
 		})
 
-		.post((req, res) => {
-						
-			if (req.body.gender === undefined || req.body.gender === '') {
-				req.flash = ('error', "Veuillez indiquer votre genre.")
-				res.redirect('/')
-			}
-			else if (req.body.name === undefined || req.body.name === '') {
-				req.flash = ('error', "Veuillez indiquer votre nom.")
-				res.redirect('/')
-			}
-			else if (req.body.firstName === undefined || req.body.firstName === '') {
-				req.flash = ('error', "Veuillez indiquer votre prénom.")
-				res.redirect('/')
-			}
-			else if (req.body.email === undefined || req.body.email === '') {
-				req.flash = ('error', "Veuillez indiquer votre email.")
-				res.redirect('/')
-			}
-			else if (req.body.orientation === undefined || req.body.orientation === '') {
-				req.flash = ('error', "Veuillez indiquer votre orientation.")
-				res.redirect('/')
-			}
-			else if (req.body.password === undefined || req.body.password === '') {
-				req.flash = ('error', "Veuillez indiquer votre mot de passe.")
-				res.redirect('/')
-			}
-			else {
-				// REQ SQL
-			}
-		})
+	app.post('/', (req, res) => {
+			console.log(req.body)
+		if (req.body.gender === undefined || req.body.gender === '') {
+			req.flash('error', "Veuillez indiquer votre genre.")
+			res.redirect('/')
+		}
+		else if (req.body.name === undefined || req.body.name === '') {
+			req.flash('error', "Veuillez indiquer votre nom.")
+			res.redirect('/')
+		}
+		else if (req.body.firstName === undefined || req.body.firstName === '') {
+			req.flash('error', "Veuillez indiquer votre prénom.")
+			res.redirect('/')
+		}
+		else if (req.body.email === undefined || req.body.email === '') {
+			req.flash('error', "Veuillez indiquer votre email.")
+			res.redirect('/')
+		}
+		else if (req.body.orientation === undefined || req.body.orientation === '') {
+			req.flash('error', "Veuillez indiquer votre orientation.")
+			res.redirect('/')
+		}
+		else if (req.body.password === undefined || req.body.password === '') {
+			req.flash('error', "Veuillez indiquer votre mot de passe.")
+			res.redirect('/')
+		}
+		else {
+			// REQ SQL
+		}
 			
+	})
 
 
 	app.route('/profile')
--- server.js
+++ server.js
@@ -19,65 +19,65 @@ app.use(session({
 	saveUninitialized: true,
 	cookie: { secure: false }
 }))
-app.use(require('./middlewares/flash.js'))
+// app.use(require('./middlewares/flash.js'))
 
 // Routes
 app.set('views', __dirname+"/views")
-// require('./routes')(app)
+require('./routes')(app)
 
 
 
 
 
-	app.get('/', function(req, res) {
-		console.log(req.session)
-		res.render('pages/index');
-	})
+// 	app.get('/', function(req, res) {
+// 		console.log(req.session)
+// 		res.render('pages/index');
+// 	})
 
-	app.post('/', function(req, res) {
+// 	app.post('/', function(req, res) {
 					
-		if (req.body.gender === undefined || req.body.gender === '') {
-			req.flash = ('error', "Veuillez indiquer votre genre.")
-			res.redirect('/')
-		}
-		else if (req.body.name === undefined || req.body.name === '') {
-			req.flash = ('error', "Veuillez indiquer votre nom.")
-			res.redirect('/')
-		}
-		else if (req.body.firstName === undefined || req.body.firstName === '') {
-			req.flash = ('error', "Veuillez indiquer votre prénom.")
-			res.redirect('/')
-		}
-		else if (req.body.email === undefined || req.body.email === '') {
-			req.flash = ('error', "Veuillez indiquer votre email.")
-			res.redirect('/')
-		}
-		else if (req.body.orientation === undefined || req.body.orientation === '') {
-			req.flash = ('error', "Veuillez indiquer votre orientation.")
-			res.redirect('/')
-		}
-		else if (req.body.password === undefined || req.body.password === '') {
-			req.flash = ('error', "Veuillez indiquer votre mot de passe.")
-			res.redirect('/')
-		}
-		else {
-			// REQ SQL
-		}
-	})
+// 		if (req.body.gender === undefined || req.body.gender === '') {
+// 			req.flash = ('error', "Veuillez indiquer votre genre.")
+// 			res.redirect('/')
+// 		}
+// 		else if (req.body.name === undefined || req.body.name === '') {
+// 			req.flash = ('error', "Veuillez indiquer votre nom.")
+// 			res.redirect('/')
+// 		}
+// 		else if (req.body.firstName === undefined || req.body.firstName === '') {
+// 			req.flash = ('error', "Veuillez indiquer votre prénom.")
+// 			res.redirect('/')
+// 		}
+// 		else if (req.body.email === undefined || req.body.email === '') {
+// 			req.flash = ('error', "Veuillez indiquer votre email.")
+// 			res.redirect('/')
+// 		}
+// 		else if (req.body.orientation === undefined || req.body.orientation === '') {
+// 			req.flash = ('error', "Veuillez indiquer votre orientation.")
+// 			res.redirect('/')
+// 		}
+// 		else if (req.body.password === undefined || req.body.password === '') {
+// 			req.flash = ('error', "Veuillez indiquer votre mot de passe.")
+// 			res.redirect('/')
+// 		}
+// 		else {
+// 			// REQ SQL
+// 		}
+// 	})
 		
 
 
-app.route('/profile')
-	.get((req, res) => {
-		res.render('pages/profile')
-	})
+// app.route('/profile')
+// 	.get((req, res) => {
+// 		res.render('pages/profile')
+// 	})
 
 
-app.route('/matcha')
-	.get((req, res) => {
-		// var i = 'xyz';
-		res.render('pages/test');
-	});
+// app.route('/matcha')
+// 	.get((req, res) => {
+// 		// var i = 'xyz';
+// 		res.render('pages/test');
+// 	});
 
 
 
